c31b778d91172fcd792f6d719b6e8da3e62ab807
nagy- <hybase@outlook.hu>
2025-01-13 18:08:05 +0100

    v1.1

--- websender/utils.py
+++ websender/utils.py
@@ -20,6 +20,37 @@ USDC_UUID = "e05735c...."
 EURC_UUID = "f19fec83...."
 
 
+
+def get_account_uuid(key_name, key_secret,):
+    request_method = "POST"
+    url = "api.coinbase.com"
+    request_path = "/v2/accounts/"
+    jwt_payload = {
+        'iss': 'cdp',
+        'nbf': int(time()),
+        'exp': int(time()) + 120,
+        'sub': key_name,
+        'uri': request_method + " " + url + request_path
+    }
+
+    # Generáljunk egy nonce-t
+    nonce = os.urandom(16).hex()
+
+    # Generáljuk a JWT-t
+    jwt_token = jwt.encode(jwt_payload, key_secret, algorithm="ES256", headers={
+        "kid": key_name,
+        "nonce": nonce
+    })
+    print(jwt_token)
+
+    # Fejlécek
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {jwt_token}"
+    }
+    response = requests.post(f"https://api.coinbase.com/v2/accounts", headers=headers)
+    print(response.text)
+
 def Usdc_sender(recipient, amount, key_name, key_secret, name):
     request_method = "POST"
     url = "api.coinbase.com"
@@ -156,21 +187,34 @@ def Eurc_sender(recipient, amount, key_name, key_secret, name):
 
 def send_from_wallets(wallets, key_name, key_secret, name):
     log = {}
+    key_secret = key_secret.replace('\\n', '\n')
+    get_account_uuid(key_name,key_secret)
+
     for wallet in wallets.split("\r\n"):
         base58_string = wallet
 
         try:
             pubkey = Keypair.from_base58_string(base58_string).pubkey()
             recipient = pubkey
+
+
+            print(pubkey)
+            print(recipient)
+            print(key_name)
+            print(key_secret)
+            print(name)
+
             usdc_amount = round(random.uniform(1, 1.79), 2)
             eurc_amount = round(random.uniform(0.15, 0.65), 2)
+
             Eurc_sender(recipient=recipient, amount=eurc_amount, key_name=key_name, key_secret=key_secret, name=name)
-            wait_time = round(random.uniform(10, 49))  # meghatározza a várakozási időt a következő iteráció előtt
-            time.sleep(wait_time)  #várakozik a következő utalás előtt
-            Usdc_sender(recipient=recipient, amount=usdc_amount, key_name=key_name, key_secret=key_secret, name=name)
-            wait_time = round(random.uniform(22, 78))  #meghatározza a várakozási időt a következő iteráció előtt
+            #wait_time = round(random.uniform(10, 49))  # meghatározza a várakozási időt a következő iteráció előtt
+            #time.sleep(wait_time)  #várakozik a következő utalás előtt
+
+            #Usdc_sender(recipient=recipient, amount=usdc_amount, key_name=key_name, key_secret=key_secret, name=name)
+            #wait_time = round(random.uniform(22, 78))  #meghatározza a várakozási időt a következő iteráció előtt
             print("nincs hiba, várakozik")
-            time.sleep(wait_time)  #várakozik a következő iteráció előtt
+            #time.sleep(wait_time)  #várakozik a következő iteráció előtt
 
         except ValueError as e:
             print(f"Hiba történt: {e}")
