47e7913c27952c2002d2035f883377e5eb628383
nagy- <hybase@outlook.hu>
2025-01-20 17:38:28 +0100

    v2.0.1 admin list_display

--- websender/admin.py
+++ websender/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 
 from websender.models import SolanaLog, SolanaUser
 
 # Register your models here.
-admin.site.register(SolanaLog)
+
+class SolanaLogAdmin(ModelAdmin):
+    list_display = ["user","fee","ip","currency","destination_address"]
+
+admin.site.register(SolanaLog,SolanaLogAdmin)
 admin.site.register(SolanaUser)
